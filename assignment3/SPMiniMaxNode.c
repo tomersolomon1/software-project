cfbd70e9aee26db453c64ca0ad78800debf7f886
Tomer Solomon <tomer.solomon1@gmail.com>
2017-05-30 16:45:30 +0300

    creating board evaluation

--- assignment3/SPMiniMaxNode.c
+++ assignment3/SPMiniMaxNode.c
@@ -12,20 +12,123 @@ typedef struct move_value_t {
 	int value; /* the value of the move, according to the mini-max algo */
 } move_value;
 
-SPFiarGame *create_board() {
-	SPFiarGame* new_board = (SPFiarGame *) malloc(sizeof(SPFiarGame));
-	for (int i = 0; i < 6; i++) {
-		for(int j = 0; j < 7; j++) {
-			new_board->gameBoard[i][j] = 2; /* defualt value, means empty */
-		}
+
+int inner_product(int v[], int u[]) {
+	int product = 0;
+	for (int i = 0; i < 7; i++) {
+		product += v[i] * u[i];
 	}
-	for (int ci = 0 ; ci < 7; ci++) {
-		new_board->tops[ci];
-	}
-	return new_board;
+	return product;
 }
 
-/* creating another copy of SPFiarGame - for MiniMax algo */
+int winning_value(int winner) {
+	int value;
+	winner == 1 ? value = INT_MAX : value = INT_MIN;
+	return value;
+}
+
+int update_sequences(int player1_counter, int player2_counter, int sequences_counter[]) {
+	int somebody_won = 0; /* 0 means no winner, 1 means player 1 won, 2 means player 2 won */
+	if (player1_counter == 4 || player2_counter == 4) {
+		player1_counter == 4 ? somebody_won = 1 : somebody_won = 2;
+	} else {
+		int sequence_index = player1_counter - player2_counter + 3;
+		sequences_counter[sequence_index] += 1;
+	}
+	return somebody_won;
+}
+
+/* compute sequences in row - to the right
+ * returning 0 means no winner, 1 means player 1 won, 2 means player 2 won
+ */
+
+int span_row_right(SPFiarGame* currentGame, int ri, int cj, int sequences_counter[]) {
+	int somebody_won = 0;
+	int player1_counter = 0;
+	int player2_counter = 0;
+	if (cj+3 < SP_FIAR_GAME_N_COLUMNS) {
+		for (int k = 0; k < 4; k++) {
+			currentGame->gameBoard[ri][cj+k] == SP_FIAR_GAME_PLAYER_1_SYMBOL ? player1_counter += 1 : player2_counter += 1;
+		}
+	}
+	return update_sequences(player1_counter, player2_counter, sequences_counter);
+}
+
+/* compute sequences in column - up-wise
+ * returning 0 means no winner, 1 means player 1 won, 2 means player 2 won
+ */
+int span_column_up(SPFiarGame* currentGame, int ri, int cj, int sequences_counter[]) {
+	int player1_counter = 0;
+	int player2_counter = 0;
+	if (ri+3 < SP_FIAR_GAME_N_ROWS) {
+		for (int k = 0; k < 4; k++) {
+			currentGame->gameBoard[ri+k][cj] == SP_FIAR_GAME_PLAYER_1_SYMBOL ? player1_counter += 1 : player2_counter += 1;
+		}
+	}
+	return update_sequences(player1_counter, player2_counter, sequences_counter);
+}
+
+/* compute sequences in diagonal - upright
+ * returning 0 means no winner, 1 means player 1 won, 2 means player 2 won
+ */
+int span_diagonal_upright(SPFiarGame* currentGame, int ri, int cj, int sequences_counter[]) {
+	int player1_counter = 0;
+	int player2_counter = 0;
+	if ((ri+3 < SP_FIAR_GAME_N_ROWS) && (cj+3 < SP_FIAR_GAME_N_COLUMNS)) {
+		for (int k = 0; k < 4; k++) {
+			currentGame->gameBoard[ri+k][cj+k] == SP_FIAR_GAME_PLAYER_1_SYMBOL ? player1_counter += 1 : player2_counter += 1;
+		}
+	}
+	return update_sequences(player1_counter, player2_counter, sequences_counter);
+}
+
+/* compute sequences in diagonal - upleft
+ * returning 0 means no winner, 1 means player 1 won, 2 means player 2 won
+ */
+int span_diagonal_upleft(SPFiarGame* currentGame, int ri, int cj, int sequences_counter[]) {
+	int player1_counter = 0;
+	int player2_counter = 0;
+	if ((ri+3 < SP_FIAR_GAME_N_ROWS) && (cj-3 >= 0)) {
+		for (int k = 0; k < 4; k++) {
+			currentGame->gameBoard[ri+k][cj-k] == SP_FIAR_GAME_PLAYER_1_SYMBOL ? player1_counter += 1 : player2_counter += 1;
+		}
+	}
+	return update_sequences(player1_counter, player2_counter, sequences_counter);
+}
+
+/*
+ * the algorithm: calculating sequences of player 1 & 2, and then sum it up with the weights vector [-5, -2, -1, 1, 2, 5]
+ * operates in O(n) complexity
+ */
+int evaluate_board(SPFiarGame* currentGame) {
+	int sequences_counter[7] = { 0 };
+	int weights[6] = {-5, -2, -1, 0, 1, 2, 5};
+	int somebody_won = 0; /* somebody_won = 1 means player 1 won, somebody_won = 2 means player 2 won */
+	for (int ri = 0; ri < SP_FIAR_GAME_N_ROWS; ri++) {
+		for (int cj = 0; cj < SP_FIAR_GAME_N_COLUMNS; cj++) {
+			somebody_won = span_diagonal_upleft(currentGame, ri, cj, sequences_counter);
+			if (somebody_won != 0) {
+				return winning_value(somebody_won);
+			}
+			span_diagonal_upright(currentGame, ri, cj, sequences_counter);
+			if (somebody_won != 0) {
+				return winning_value(somebody_won);
+			}
+			span_row_right(currentGame, ri, cj, sequences_counter);
+			if (somebody_won != 0) {
+				return winning_value(somebody_won);
+			}
+			span_column_up(currentGame, ri, cj, sequences_counter);
+			if (somebody_won != 0) {
+				return winning_value(somebody_won);
+			}
+		}
+	}
+	return inner_product(sequences_counter, weights);
+}
+
+/* creating another copy of SPFiarGame - for MiniMax algo
+ * to be deleted - duplicate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
 SPFiarGame* copy_board(SPFiarGame* game) {
 	SPFiarGame* new_game = (SPFiarGame *) malloc(sizeof(SPFiarGame));
 	if (new_game == NULL) {
@@ -45,10 +148,11 @@ SPFiarGame* copy_board(SPFiarGame* game) {
 }
 
 int spMinimaxSuggestMove(SPFiarGame* currentGame, unsigned int maxDepth) {
-	SPFiarGame* game_copy = copy_board(currentGame);
+	SPFiarGame* game_copy = spFiarGameCopy(currentGame);
 	if (game_copy != NULL) {
 		//if (game_is_not_over) - should implement
 		move_value best_move  = minimaxAlgo(game_copy);
+		spFiarGameDestroy(game_copy);
 		if (best_move.move != -1) {
 			return best_move.move;
 		} else {
@@ -76,13 +180,13 @@ void update_best_move(move_value* this_move, int value, int ci, char current_pla
  */
 int is_game_over(SPFiarGame* game_copy, move_value* this_move) {
 	char game_status = spFiarCheckWinner(game_copy);
-	if (game_status == SP_FIAR_GAME_PLAYER_1_SYMBOL) {
+	if (game_status == SP_FIAR_GAME_PLAYER_1_SYMBOL) { /* player 1 has won */
 		this_move = INT_MAX;
 		return -1;
-	} else if (game_status == SP_FIAR_GAME_PLAYER_2_SYMBOL) {
+	} else if (game_status == SP_FIAR_GAME_PLAYER_2_SYMBOL) { /* player 2 has won */
 		this_move = INT_MIN;
 		return -1;
-	} else if (game_status == SP_FIAR_GAME_TIE_SYMBOL) {
+	} else if (game_status == SP_FIAR_GAME_TIE_SYMBOL) { /* it's a tie */
 		this_move = 0;
 		return -1;
 	}
@@ -92,7 +196,7 @@ int is_game_over(SPFiarGame* game_copy, move_value* this_move) {
 
 /* assuming maxDepth >= 1
  * the object of player 1 is to maximize the value-function, and the object of player 2 is to minimize the value-function
- * by default, |this_move.value| < INT_MAX, since value = INT_MAX is means winning of player 1 (similarly, INT_MIN means winning of player 2) */
+ * by default, |this_move.value| < INT_MAX, since value = INT_MAX is means the winning of player 1 (similarly, INT_MIN means the winning of player 2) */
 move_value minimaxAlgo(SPFiarGame* game_copy, unsigned int maxDepth) {
 	char current_symbol; /* the symbol of the current player */
 	move_value this_move; /* will contain the best move and it's value */
